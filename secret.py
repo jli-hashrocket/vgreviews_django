db1985633a50c42ca828e84450ac9e6b9b03163c
macheteshuffle <jeff.li029@gmail.com>
2014-07-16 22:13:20 -0400

    corrected syntax for settings

--- secret.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = '+nq0=95p--7n_vff^en&++5#71+*ff_#021f68^bm48!t750c@'
--- vgreviews/settings.py
+++ vgreviews/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'vgreviews',
-        'HOST': '127.0.0.1'
+        'HOST': '127.0.0.1',
         'PORT': 5432
     }
 }
