6de38e4077b5eb46555085aedb9d166bf4fae39b
macheteshuffle <jeff.li029@gmail.com>
2014-07-16 23:29:53 -0400

    changing static root for production

--- vgreviews/settings.py
+++ vgreviews/settings.py
@@ -99,5 +99,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join('/Users/jli/Dropbox/launchacademy/django_apps/vgreviews/apps/reviews','static')
+# STATIC_ROOT = os.path.join('/Users/jli/Dropbox/launchacademy/django_apps/vgreviews/apps/reviews','static')
+STATIC_ROOT = os.path.join('vgreviews/apps/reviews','static')
+
 STATIC_URL = '/static/'
